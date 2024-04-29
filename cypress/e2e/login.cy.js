4b2896d0292d5f18d9f768398e14af89557d618f
GessicaVieira <gessicaflx@hotmail.com>
2024-04-29 18:55:03 -0300

    Modificações na estrutura do projeto

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,43 @@
+//hooks
+    //before - antes de todos os testes
+    //beforeEach - antes de cada teste
+    //after - depois de todos os testes
+    //afterEach - depois de cada teste
+
+const element = require("../fixtures/login.json")
+
+beforeEach(() => {
+    cy.visit('https://automacao.qacoders-academy.com.br/login');
+});
+
+afterEach(() => {
+    cy.screenshot();
+})
+
+context('Login', () => {
+
+    it('Login com sucesso', () => {
+        const email = Cypress.env('EMAIL');
+        const password = Cypress.env('PASSWORD');
+        cy.Login(email, password);
+    });
+
+
+    it('Login com e-mail válido e a senha inválida', () => {
+        cy.get(element.input_email).type(Cypress.env('EMAIL'));
+        cy.get(element.input_password).type(Cypress.env('PASSOWORD_INVALIDO'));
+        cy.get(element.btn_login).click();
+        cy.MsgLoginFalho()
+
+    });
+
+
+    it('Login com e-mail inválido e a senha válida', () => {
+        cy.get(element.input_email).type('sysadminn@qacoders.com');
+        cy.get(element.input_password).type(Cypress.env('PASSWORD'));
+        cy.get(element.btn_login).click();
+        cy.MsgLoginFalho()
+    });
+
+    
+});
--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Login de acesso', () => {
-  it('passes', () => {
-    cy.visit('https://automacao.qacoders-academy.com.br/login')
-    cy.contains("Login")
-  })
-})
